79e073c2c04ad0c3f5158d551a02c58dd81bafd0
Pavan Kumar <pavankumarts2001@gmail.com>
2022-06-27 21:30:28 +0530

    dp done

--- app/src/main/java/com/example/collegeproject/ProfileActivity.java
+++ app/src/main/java/com/example/collegeproject/ProfileActivity.java
@@ -1,14 +1,107 @@
 package com.example.collegeproject;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    StorageReference storageRef;
+    Button addImg, btnUplaod;
+    private static int RESULT_LOAD_IMAGE = 1;
+    Uri imageUri;
+    ImageView imageView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        imageView = findViewById(R.id.imageView);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+        addImg = findViewById(R.id.btnAddImg);
+        btnUplaod = findViewById(R.id.btnProfileUplaod);
+        addImg.setOnClickListener(v -> {
+            selectImage();
+        });
+        btnUplaod.setOnClickListener(v -> {
+            profileUpload();
+        });
+    }
+
+    private void profileUpload() {
+        progressBar.setVisibility(View.VISIBLE);
+        storageRef = FirebaseStorage.getInstance().getReference("myImages/" + UUID.randomUUID().toString());
+        storageRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Log.d(TAG, "uri"+uri);
+                                Map<String, Object> image = new HashMap<>();
+                                image.put("uri",uri);
+                                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                DatabaseReference reference = database.getReference("imageURL");
+                                String uid = user.getUid();
+                                reference.child(uid).setValue(image);
+                                Toast.makeText(getApplicationContext(), "Successfully uploaded", Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.INVISIBLE);
+                                imageView.setImageURI(null);
+                            }
+                        });
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void selectImage() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(i,100);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && data != null && data.getData() != null){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
     }
 }
